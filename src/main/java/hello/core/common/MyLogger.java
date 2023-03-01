04fdc5ecda1cfbac4d5f710332333505836db5de
seogyugim <asap0208@gmail.com>
2023-03-01 23:08:02 +0900

    ~62강

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,34 @@
+package hello.core.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+@Getter
+@Setter
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    public void log(String msg) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + msg);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + "request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + "request scope bean destroy: " + this);
+    }
+}
